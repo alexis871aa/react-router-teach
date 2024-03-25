9185985ad46185addafa2d765078fa3c3d1e90b3
Aleksey Sokolov <alexis871aa@yandex.ru>
2024-03-25 17:59:55 +0300

    modified files app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,78 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, NavLink, Outlet, useParams, useMatch } from 'react-router-dom';
 import styles from './app.module.css';
 
+const fetchProductList = () => [
+	{ id: 1, name: 'Телевизор' },
+	{ id: 2, name: 'Смартфон' },
+	{ id: 3, name: 'Планшет' },
+];
+
+const fetchProduct = (id) =>
+	({
+		1: { id: 1, name: 'Телевизор', price: 29900, amount: 15 },
+		2: { id: 2, name: 'Сматрфон', price: 13900, amount: 48 },
+		3: { id: 3, name: 'Планшет', price: 18400, amount: 23 },
+	})[id];
+
 const MainPage = () => <div>Контент главной страницы</div>;
-const Catalog = () => <div>Контент каталога</div>;
+const Catalog = () => (
+	<div>
+		<h3>Каталог товаров</h3>
+		<ul>
+			{fetchProductList().map(({ id, name }) => {
+				return (
+					<li key={id}>
+						<NavLink to={`product/${id}`}>{name}</NavLink>
+					</li>
+				);
+			})}
+		</ul>
+		<Outlet />
+	</div>
+);
+
+const ProductNotFound = () => <div>Такой товар не существует</div>;
+
+const Product = () => {
+	const params = useParams();
+	const urlMatchData = useMatch('/catalog/:type/:id');
+	console.log('urlMatchData', urlMatchData.params.type);
+
+	const product = fetchProduct(params.id);
+
+	if (!product) {
+		return <ProductNotFound />;
+	}
+
+	const { name, price, amount } = product;
+
+	return (
+		<div>
+			<h3>Товар - {name}</h3>
+			<div>Цена: {price}</div>
+			<div>На складе: {amount}</div>
+		</div>
+	);
+};
+
 const Contacts = () => <div>Контент контактов</div>;
+
+const NotFound = () => <div>Такая страница не существует</div>;
+
+const ExtendedLink = ({ to, children }) => (
+	<NavLink to={to}>
+		{({ isActive }) =>
+			isActive ? (
+				<>
+					<span>{children}</span>
+					<span>*</span>
+				</>
+			) : (
+				children
+			)
+		}
+	</NavLink>
+);
 
 export const App = () => {
 	return (
@@ -12,20 +81,24 @@ export const App = () => {
 				<h3>Меню</h3>
 				<ul>
 					<li>
-						<Link to="/">Главная</Link>
+						<ExtendedLink to="/">Главная</ExtendedLink>
 					</li>
 					<li>
-						<Link to="/catalog">Каталог</Link>
+						<ExtendedLink to="/catalog">Каталог</ExtendedLink>
 					</li>
 					<li>
-						<Link to="/contacts">Контакты</Link>
+						<ExtendedLink to="/contacts">Контакты</ExtendedLink>
 					</li>
 				</ul>
 			</div>
 			<Routes>
-				<Route path="/" element={<MainPage />}></Route>
-				<Route path="/catalog" element={<Catalog />}></Route>
-				<Route path="/contacts" element={<Contacts />}></Route>
+				<Route path="/" element={<MainPage />} />
+				<Route path="/catalog" element={<Catalog />}>
+					<Route path="product/:id" element={<Product />} />
+					<Route path="service/:id" element={<Product />} />
+				</Route>
+				<Route path="/contacts" element={<Contacts />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
 	);
